ea0da0afd0bc5bda5599972dba24f2596ba377a6
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2021-11-18 20:32:17 +0300

    -

--- trade-spot-scalper-strategy1/src/main/java/org/velikokhatko/stratery1/services/api/exchange/SymbolInfoShort.java
+++ trade-spot-scalper-strategy1/src/main/java/org/velikokhatko/stratery1/services/api/exchange/SymbolInfoShort.java
@@ -7,10 +7,12 @@ public final class SymbolInfoShort {
     public final String symbol;
     public final String baseAsset;
     public final double lotSizeMin;
+    public final double marketLotSizeMin;
 
     public SymbolInfoShort(SymbolInfo symbolInfo) {
         this.symbol = symbolInfo.getSymbol();
         this.baseAsset = symbolInfo.getBaseAsset();
         this.lotSizeMin = Double.parseDouble(symbolInfo.getSymbolFilter(FilterType.LOT_SIZE).getMinQty());
+        this.marketLotSizeMin = Double.parseDouble(symbolInfo.getSymbolFilter(FilterType.MARKET_LOT_SIZE).getMinQty());
     }
 }
