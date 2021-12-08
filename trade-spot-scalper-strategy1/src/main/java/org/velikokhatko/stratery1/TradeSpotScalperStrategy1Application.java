ade27cc40ab6b21ad37ca4020c6b909ba6d97ef0
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2021-12-08 13:06:36 +0300

    -

--- trade-spot-scalper-strategy1/src/main/java/org/velikokhatko/stratery1/TradeSpotScalperStrategy1Application.java
+++ trade-spot-scalper-strategy1/src/main/java/org/velikokhatko/stratery1/TradeSpotScalperStrategy1Application.java
@@ -3,18 +3,10 @@ package org.velikokhatko.stratery1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class TradeSpotScalperStrategy1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(TradeSpotScalperStrategy1Application.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
     }
 }
